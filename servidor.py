7827c4f09320942d3f49643f239fd84dfd6ab280
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2026-09-06 18:36:26 -0300

    feat: banco_estrutura

---
+++ servidor.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from dotenv import load_dotenv
+from flask import Flask
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+def conectar_banco():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
+def criar_tabela():
+    conn = conectar_banco()
+    cursor = conn.cursor()
+
+    try:
+        with open("imoveis.sql", encoding="utf-8") as arquivo:
+            comandos_sql = arquivo.read()
+
+        cursor.execute(comandos_sql)
+
+        # Processa todos os comandos/resultados do arquivo SQL
+        while True:
+            if cursor.with_rows:
+                cursor.fetchall()
+
+            if not cursor.nextset():
+                break
+
+        conn.commit()
+        print("Tabelas criadas com sucesso.")
+
+    except mysql.connector.Error as erro:
+        conn.rollback()
+        print(f"Erro ao criar as tabelas: {erro}")
+
+    finally:
+        cursor.close()
+        conn.close()
+
+    if __name__ == "__main__":
+        criar_tabela()
+        app.run(debug=True)
+
---
+++ test.py
@@ -0,0 +1,26 @@
+import pytest 
+from unittest.mock import MagicMock, patch
+from api import app 
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+@patch("api.conectar_banco")
+def test_deletar_imovel(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.rowcount = 1
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.delete("/imovel/1")
+
+    assert response.stauts_code == 200
+    assert response.get_json() == {"Mensagem": "imóvel excluído com sucesso"}
+
+    mock_cursor.execute.assert_called_once_with("DELETE FROM ")
