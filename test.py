f10fe8ff730ba6dedeaf0202315fe8bafade6f0e
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2026-09-07 01:19:03 -0300

    test: busca_tipo_ok & busca_tipo_vazio

--- test.py
+++ test.py
@@ -43,10 +43,58 @@ def test_deletar_imovel_inexistente(mock_conectar_banco, client):
     assert response.status_code == 404
     assert response.get_json() == {"erro": "Imóvel não encontrado"}
 
-    mock_cursor.execute.assert_called_once_with("DELETE FROM imoveis WHERE id = % ", (99999,))
+    mock_cursor.execute.assert_called_once_with("DELETE FROM imoveis WHERE id = %s ", (99999,))
 
     mock_conn.commit.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
 
+@patch("api.conectar_banco")
+def test_busca_tipo_ok(mock_conectar_banco, client, tipo):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchall.return_value = [(1,'Nicole Common', 'Travessa', 'Lake Danielle', 'Judymouth', '85184', 'casa em condominio', 488423.52, '2017-07-29'),
+                                         (2, 'Price Prairie', 'Travessa', 'Colonton', 'North Garyville', '93354', 'casa em condominio', 260069.89, '2021-11-30'),
+                                         (3, 'Taylor Ranch', 'Avenida', 'West Jennashire', 'Katherinefurt', '51116', 'apartamento', 815969.92, '2020-04-24'),
+                                         (4, 'Stacey Isle', 'Avenida', 'Reneeberg', 'Bentleymouth', '01631', 'terreno', 352507.35, '2014-11-03')]
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get_json('/imovel/tipo')
+
+    assert response.status_code == 200
+    assert response.get_json == [{'id':1, 'logradouro':'Nicole Common', 'tipo_logradouro':'Travessa', 'bairro':'Lake Danielle', 'cidade':'Judymouth', 'cep':'85184','tipo': 'casa em condominio', 'valor': 488423.52, 'data_aquisicao':'2017-07-29'},
+                                 {'id': 2, 'logradouro':'Price Prairie', 'tipo_logradouro':'Travessa', 'bairro':'Colonton', 'cidade':'North Garyville', 'cep':'93354', 'tipo':'casa em condominio', 'valor':260069.89, 'data_aquisicao':'2021-11-30'}]
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", (tipo,)
+    )
+
+    mock_conn.commit.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+@patch('api.conectar_banco')
+def test_busca_tipo_vazio(mock_conectar_banco, client, tipo):
+    mock_cursor = MagicMock()
+    mock_conn = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchall.return_value = []
+
+    mock_conectar_banco.return_value = mock_conn
+    response = client.get("/imovel/tipo")
+
+    assert response.status_code == 200
+    assert response.get_json() == []
+
+    mock_cursor.execute.assert_called_once_with(
+            "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", (tipo,)
+        )
+    
+    mock_conn.commit.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
     
