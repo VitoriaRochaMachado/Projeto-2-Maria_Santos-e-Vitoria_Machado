db3aeee867a478c5746213424bd1a327175ecce6
Maria Eduarda dos Santos <mariaes3@al.insper.edu.br>
2026-09-07 01:59:35 -0300

    test: busca_cidade_ok & busca_cidade_vazio

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ def test_busca_tipo_ok(mock_conectar_banco, client, tipo):
                                  {'id': 2, 'logradouro':'Price Prairie', 'tipo_logradouro':'Travessa', 'bairro':'Colonton', 'cidade':'North Garyville', 'cep':'93354', 'tipo':'casa em condominio', 'valor':260069.89, 'data_aquisicao':'2021-11-30'}]
 
     mock_cursor.execute.assert_called_once_with(
-        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", (tipo,)
+        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", ('tipo_logradouro',)
     )
 
     mock_conn.commit.assert_called_once()
@@ -77,7 +77,7 @@ def test_busca_tipo_ok(mock_conectar_banco, client, tipo):
     mock_conn.close.assert_called_once()
 
 @patch('api.conectar_banco')
-def test_busca_tipo_vazio(mock_conectar_banco, client, tipo):
+def test_busca_tipo_vazio(mock_conectar_banco, client):
     mock_cursor = MagicMock()
     mock_conn = MagicMock()
     mock_conn.cursor.return_value = mock_cursor
@@ -91,10 +91,57 @@ def test_busca_tipo_vazio(mock_conectar_banco, client, tipo):
     assert response.get_json() == []
 
     mock_cursor.execute.assert_called_once_with(
-            "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", (tipo,)
+            "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", ('tipo_logradouro',)
         )
     
     mock_conn.commit.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
+
+@patch("api.conectar_banco")
+def test_busca_cidade_ok(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchall.return_value = [(1,'Nicole Common', 'Travessa', 'Lake Danielle', 'Judymouth', '85184', 'casa em condominio', 488423.52, '2017-07-29'),
+                                         (2, 'Price Prairie', 'Travessa', 'Colonton', 'North Garyville', '93354', 'casa em condominio', 260069.89, '2021-11-30'),
+                                         (3, 'Taylor Ranch', 'Avenida', 'West Jennashire', 'Katherinefurt', '51116', 'apartamento', 815969.92, '2020-04-24'),
+                                         (4, 'Stacey Isle', 'Avenida', 'Reneeberg', 'Bentleymouth', '01631', 'terreno', 352507.35, '2014-11-03')]
+
+    mock_conectar_banco.return_value = mock_conn
+
+    response = client.get_json('/imovel/cidade')
+
+    assert response.status_code == 200
+    assert response.get_json == [{'id':1, 'logradouro':'Nicole Common', 'tipo_logradouro':'Travessa', 'bairro':'Lake Danielle', 'cidade':'Judymouth', 'cep':'85184','tipo': 'casa em condominio', 'valor': 488423.52, 'data_aquisicao':'2017-07-29'},]
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", ('Judymouth',)
+    )
+
+    mock_conn.commit.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+@patch('api.conectar_banco')
+def test_busca_cidade_vazio(mock_conectar_banco, client):
+    mock_cursor = MagicMock()
+    mock_conn = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.fetchall.return_value = []
+
+    mock_conectar_banco.return_value = mock_conn
+    response = client.get("/imovel/cidade")
+
+    assert response.status_code == 200
+    assert response.get_json() == []
+
+    mock_cursor.execute.assert_called_once_with(
+            "SELECT logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE tipo = %s", ('JudymouthMM',)
+        )
     
+    mock_conn.commit.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
